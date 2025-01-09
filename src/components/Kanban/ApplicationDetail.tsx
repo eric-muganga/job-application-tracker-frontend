78991e5490d1f09f20601c777262c877d35e2747
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-09 14:17:59 +0100

    feat(ApplicationDetail): Enhance modal interaction for job applications and implemented the handleEditClick, handleDeleteClick

--- src/components/Kanban/ApplicationDetail.tsx
+++ src/components/Kanban/ApplicationDetail.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import { JobApplication } from "../../../store/jobApplicationsSlice";
+import { format } from "date-fns";
+
+import {
+  deleteApplication,
+  JobApplication,
+} from "../../../store/jobApplicationsSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../../store/store";
+import { useNavigate } from "react-router-dom";
 
 interface ApplicationDetailProps {
   application: JobApplication;
@@ -11,7 +19,10 @@ const ApplicationDetail: React.FC<ApplicationDetailProps> = ({
   application,
   onClose,
 }) => {
+  // Create a ref to the modal element
   const modalRef = useRef<HTMLDivElement>(null);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
 
   // Function to handle clicks outside the modal
   const handleClickOutside = (event: MouseEvent) => {
@@ -29,11 +40,58 @@ const ApplicationDetail: React.FC<ApplicationDetailProps> = ({
     };
   }, []);
 
+  // Helper function to format `jobDescription` with line breaks
+  const formatText = (text: string | undefined) =>
+    text?.split("\n").map((line, index) => (
+      <div key={index} className="mb-2">
+        {" "}
+        {/* Use div here instead */}
+        {line}
+      </div>
+    ));
+
+  // Function to handle the edit button click
+  const handleEditClick = () => {
+    navigate("/application/edit", { state: application });
+  };
+
+  // Function to handle the delete button click
+  const handleDeleteClick = async () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this application?"
+    );
+    if (confirmed) {
+      try {
+        await dispatch(deleteApplication({ id: application.id }));
+
+        onClose(); // Close the modal
+      } catch (error) {
+        console.error("Failed to delete application", error);
+      }
+    }
+  };
+
+  if (!application) {
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-40">
+        <div className="bg-white rounded-lg shadow-lg p-6 w-[90%] max-w-md">
+          <p className="text-center text-gray-500">Application not found.</p>
+          <button
+            onClick={onClose}
+            className="bg-indigo-500 text-white mt-4 px-4 py-2 rounded-md"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-40">
       <div
         ref={modalRef}
-        className="bg-white rounded-lg shadow-lg p-6 w-[90%] max-w-md"
+        className="bg-white rounded-lg shadow-lg p-6 w-[90%] max-w-md overflow-y-auto"
       >
         {/* Header Section */}
         <div className="flex justify-between items-center mb-4">
@@ -78,10 +136,10 @@ const ApplicationDetail: React.FC<ApplicationDetailProps> = ({
             </span>
           </p>
           {application.applicationDate && (
-            <p>
+            <div>
               <span className="font-semibold text-gray-700">Applied On:</span>{" "}
-              {application.applicationDate}
-            </p>
+              {format(new Date(application.applicationDate), "PPpp")}
+            </div>
           )}
           {application.contractType && (
             <p>
@@ -97,14 +155,16 @@ const ApplicationDetail: React.FC<ApplicationDetailProps> = ({
                 Job Description:
               </span>{" "}
               <br />
-              {application.jobDescription}
+              <div className="mt-2 text-gray-600 whitespace-pre-line leading-6 max-h-72 overflow-y-auto border border-gray-200 rounded-lg p-4 bg-gray-50">
+                {formatText(application.jobDescription)}
+              </div>
             </p>
           )}
           {application.interviewDate && (
             <p>
               <span className="font-semibold text-gray-700">
                 Interview Date:
-              </span>{" "}
+              </span>
               {application.interviewDate}
             </p>
           )}
@@ -119,9 +179,29 @@ const ApplicationDetail: React.FC<ApplicationDetailProps> = ({
               <span className="font-semibold text-gray-700">
                 Financial Information:
               </span>{" "}
-              {application.financialInformation}
+              <br />
+              {application.financialInformation.salary}{" "}
+              {application.financialInformation.currency} (
+              {application.financialInformation.salaryType}) -{" "}
+              {application.financialInformation.typeOfEmployment}
             </p>
           )}
+        </div>
+
+        {/* Buttons Section */}
+        <div className="mt-6 flex justify-between">
+          <button
+            onClick={handleEditClick}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none"
+          >
+            Edit
+          </button>
+          <button
+            onClick={handleDeleteClick}
+            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none"
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>

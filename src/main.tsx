4ff33d47a42833f82bdfdb52fcdddf0126829467
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-13 21:31:23 +0100

    chore(main.tsx): Update app initialization and router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import store from "../store/store.ts";
+import axios from "axios";
+
+const token = localStorage.getItem("authToken");
+if (token) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
